25e852224e0c63c3f893569627fc2b5c5b2aca90
Melih DİNÇER <m_dincer41@hotmail.com>
2025-05-26 20:43:52 +0300

    26 Mayıs third commit. -Material UI paketleri yüklendi. Mui test button eklendi.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import ButtonUsage from "../components/ButtonUsage";
 
 export default function MainLayout() {
   return (
     <div className="container">
+      <ButtonUsage />
       <h1>Main Layout</h1>
       <Outlet />
     </div>
