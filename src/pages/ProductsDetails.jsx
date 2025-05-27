f4a49c6e29432175b4779114c365ec8cd9cea5dc
Melih DİNÇER <m_dincer41@hotmail.com>
2025-05-27 23:33:47 +0300

    27 Mayıs second commit.

--- src/pages/ProductsDetails.jsx
+++ src/pages/ProductsDetails.jsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import ProductItem from "../components/ProductItem";
+import Loading from "../components/Loading";
+
 export default function ProductsDetailsPage() {
-  return <h1>ProductsDetails:</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    async function fetchProductDetails() {
+      try {
+        const response = await fetch("http://localhost:5000/products/" + id);
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProductDetails();
+  }, [id]);
+
+  if (loading) {
+    return <Loading message="Yükleniyor..." />;
+  }
+
+  return <ProductItem product={product} />;
 }
